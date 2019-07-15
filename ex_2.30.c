48b599732a2c7656287584191ae41b4f98117bbf
Natinmayhaus <egmntrkylmz@gmail.com>
2019-07-15 22:22:24 +0300

    Solution for exercise 2.24 - Deitel, C How to Program 6th ed

---
+++ ex_2.30.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*  A program that inputs one five-digit number, separates the number into its individual digits
+ *  and prints the digits separated from one another by three spaces each.  */
+
+int main() {
+
+    //  define variables
+    int num;
+
+    //  prompt user to enter a five-digit number
+    printf("Enter a five-digit number:");
+
+    //  read the number
+    scanf("%d", &num);
+
+    //  control whether the number has five digits or not
+    if((num/10000) > 9){
+        printf("The number has more than five digits. Aborting...\n");
+        return 0;
+    }
+    if((num/10000) < 10) {
+        //  prompt the digits
+        printf("%d   %d   %d   %d   %d\n",
+               (num / 10000), (num % 10000) / 1000,
+               (num % 1000) / 100, (num % 100) / 10, (num % 10));
+    }
+
+    return 0;
+} /*  END of main  */
