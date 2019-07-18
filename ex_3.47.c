287b6e9b27a0e9162f7c11381441f9a81cd86088
Natinmayhaus <egmntrkylmz@gmail.com>
2019-07-18 14:48:06 +0300

    Solution for exercise 3.47 - Deitel, C How to Program

---
+++ ex_3.47.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*  An application that inputs the current world population and the annual population growth rate
+ *  then displays the estimated world population after one, two, three, four and five years.  */
+
+int main() {
+
+    //  define variables
+    unsigned long long int  population;
+    int count = 1;
+    float growthRate;
+
+    //  prompt user to enter necessary values and read it
+    printf("Enter the current world population:");
+    scanf("%llu", &population);
+    printf("Enter the annual population growth rate:");
+    scanf("%f", &growthRate);
+    printf("\n");
+
+    //  calculate and display the estimated world population
+    //  for the next five years.
+    while(count <= 5){
+        population = (unsigned long long int) (population + (population * (growthRate/100)));
+        printf("Estimated world population for year %d:\t%llu\n", count, population);
+        ++count;
+    }
+
+
+    return 0;
+}   /*  END of main  */
