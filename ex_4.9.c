8df68ac83b70b0c4579d5c4f41f8405acab78c55
Natinmayhaus <egmntrkylmz@gmail.com>
2019-10-01 14:08:52 +0300

    Solution for exercise 4.9 - Deitel, C How to Program 6th ed

---
+++ ex_4.9.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*  A program that sums a sequence of integers.  */
+
+int main() {
+
+    //  define variables
+    int count;
+    int seqSize;
+    int number;
+    int sum;
+
+    //  input the count then the numbers
+    scanf("%d", &seqSize);
+
+    //  loop through the sequence and calculate the sum
+    for(count = 1, sum = 0; count <= seqSize; count++){
+        scanf("%d", &number);
+        sum += number;
+    }
+
+    //  print the result
+    printf("\nSum is %d.", sum);
+
+    return 0;
+}
